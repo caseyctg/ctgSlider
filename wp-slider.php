1953e4fcf19f96c7a45ed7976537f5cd18031253
caseyctg <caseygovero@caseys-MacBook-Pro-3.local>
2015-01-21 21:43:21 -0600

    Fixing wordpress slider code

--- wp-slider.php
+++ wp-slider.php
@@ -1,3 +1,6 @@
+<!-- 
+Set a category of posts to slider and put an image in the content.
+-->
 <div id="ctgslider">	
       <?php 
             wp_reset_query(); 
@@ -7,8 +10,14 @@
             <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
             <div class="Caption"><?php the_title(); ?></div>
-            <?php the_content(); ?>
-            </a>
+              
+            <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                    <?php echo $image[0]; ?>
+                </a>
+            <?php endif; ?>
+    
             </div>
                   <?php endwhile; ?>
                   <?php else : ?>
